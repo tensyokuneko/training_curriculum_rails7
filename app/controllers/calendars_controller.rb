d859f7df084df856c94c0248647b7bd6601cacb5
tensyokuneko <npon.fafa@gmail.com>
2025-07-06 14:09:39 +0900

    Update calendars_controller.rb

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,7 +36,7 @@ class CalendarsController < ApplicationController
       end
 
       wday_num = Date.today.wday + x
-      if #「wday_numが7以上の場合」という条件式
+      if wday_num >= 7
         wday_num = wday_num -7
       end
       
